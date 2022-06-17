aaccfdb7c89192b918db4c1c2a98e107fccc4f09
starwalker00 <aaronwalkerup@gmail.com>
2022-06-17 14:21:07 +0200

    deploy solmate NFT

---
+++ .env.example.sh
@@ -0,0 +1,3 @@
+export RPC_URL=https://polygon-mumbai.g.alchemy.com/v2/aaaaaaaaaaaaaaaaaaaaaaa
+export ETHERSCAN_API_KEY=AAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+export PRIVATE_KEY=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
