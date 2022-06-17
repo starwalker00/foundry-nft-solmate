4edbb1e7b87f3e74ed73a1ce2f012a83e7340c7e
starwalker00 <aaronwalkerup@gmail.com>
2022-06-17 15:08:11 +0200

    add mintMany.py cast script

---
+++ mintMany.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Module Docstring
+"""
+
+__author__ = "Your Name"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+import subprocess
+
+def main():
+    """ Main entry point of the app """
+    for x in range(1, 4):
+        print(f"Iteration {x}")
+        # build command
+        envCmd = ". ./.env.sh"
+        castCmd = "cast send --rpc-url=$RPC_URL 0xb3de220de2488071fc362141a939fb6689e0c52c \"mintTo(address)\" 0x235596F35fdeAc45a59bf38640dD68F19A85dE39 --private-key=$PRIVATE_KEY"
+        fullCmd = [envCmd + "&&" + castCmd]
+        proc = subprocess.Popen(fullCmd,stdout=open('python_logs.txt', 'a'),stderr=subprocess.PIPE, text=True, shell=True)
+
+        # handle stuck proc/tx with timeout
+        try:
+            outs, errs = proc.communicate(timeout=15)
+        except subprocess.TimeoutExpired:
+            proc.kill()
+            outs, errs = proc.communicate()
+
+        # handle errors, break on first one
+        if len(errs)>0:
+            print("ERROR---------------------")
+            print(errs)
+            return -1
+
+    print("ALL DONE")
+    return 0
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
+
